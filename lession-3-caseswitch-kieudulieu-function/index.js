9deae78c95034cf54130e9cf0e4afb00ecc203d5
hoangnm <hoangnm.ndm@gmail.com>
2023-03-10 09:07:42 +0700

    add BTVN

--- lession-3-caseswitch-kieudulieu-function/index.js
+++ lession-3-caseswitch-kieudulieu-function/index.js
@@ -47,12 +47,54 @@
 //   console.log(`${testNumber} là một số chẵn`);
 // } else console.log(`${testNumber} không là một số chẵn`);
 
-function testNumber(a) {
-  if (a % 2 === 0) {
-    console.log(`${a} là một số chẵn`);
-  } else console.log(`${a} không là một số chẵn`);
+// function testNumber(a) {
+//   if (a % 2 === 0) {
+//     console.log(`${a} là một số chẵn`);
+//   } else console.log(`${a} không là một số chẵn`);
+// }
+// testNumber(100);
+// testNumber(102);
+// testNumber(1902323233454);
+// testNumber(123456789054678);
+
+/**
+ * Nhập vào số điện của một gia đình và tính ra số tiền điện của gia đình đó biết rằng:
+ * 1. 50 số đầu có giá 1000vnd
+ * 2. 25 số tiếp theo có giá 1200vnd
+ * 3. 25 số tiếp theo có giá 1500vnd
+ * 4. sau 100 số, những số điện còn lại có giá 2000vnd 1 số.
+ * Viết dưới dạng function
+ */
+
+function tinhTienDien(soDien) {
+  if (soDien <= 50) {
+    // console.log(soDien * 1000);
+    return soDien * 1000;
+  } else if (soDien <= 75) {
+    // console.log(50 * 1000 + (soDien - 50) * 1200);
+    return 50 * 1000 + (soDien - 50) * 1200;
+  } else if (soDien <= 100) {
+    // console.log(50 * 1000 + 25 * 1200 + (soDien - 75) * 1500);
+    return 50 * 1000 + 25 * 1200 + (soDien - 75) * 1500;
+  } else {
+    // console.log(50 * 1000 + 25 * 1200 + 25 * 1500 + (soDien - 100) * 2000);
+    return 50 * 1000 + 25 * 1200 + 25 * 1500 + (soDien - 100) * 2000;
+  }
 }
-testNumber(100);
-testNumber(102);
-testNumber(1902323233454);
-testNumber(123456789054678);
+console.log(`Tien dien nha ban la: `, tinhTienDien(100));
+
+function tinhTienDienTheoHoGiaDinh(soDien, tenHoGiaDinh) {
+  console.log(`Tien dien gia dinh ${tenHoGiaDinh} la: `, tinhTienDien(soDien));
+}
+
+tinhTienDienTheoHoGiaDinh(12345, "Đại gia vip pro tại HN");
+let today = "Fri";
+switch (today) {
+  case "Sat":
+  case "Sun":
+    console.log("Hom nay la cuoi tuan");
+    break;
+  default:
+    console.log("Hom nay ko phai la cuoi tuan");
+  //
+}
