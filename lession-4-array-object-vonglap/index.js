b6f247a047dd6174e4ab37aadebc8ddd3f1014ec
hoangnm <hoangnm.ndm@gmail.com>
2023-03-13 08:35:59 +0700

    lession 4

---
+++ lession-4-array-object-vonglap/index.js
@@ -0,0 +1,61 @@
+/**
+ * Array
+ */
+
+// const myStudent = ["Hoang", "Cuong", "Dung", "Duyen", "Cuong", "Hai"];
+// console.log(myStudent.length);
+
+// console.log(myStudent[1]);
+// console.log(myStudent.indexOf("Cuong"));
+// console.log(myStudent.lastIndexOf("Cuong"));
+
+// // myStudent[-1] = "obama";
+// // console.log(myStudent[-1]);
+// // console.log(myStudent.indexOf("Donal Trum"));
+// myStudent.push("Huong"); // thêm 1 phần tử cuối mảng
+// // console.log(myStudent);
+// myStudent.pop(); // xoá 1 phần tử ở cuối mảng
+// myStudent.shift(); // xoá 1phần tử ở đầu mảng
+// myStudent.unshift(); // thêm 1 phần tử đầu mảng.
+// myStudent.slice(); // cắt một phần của mảng và tạo ra mảng mới
+// myStudent.splice(); //cắt và thay thế
+
+// const animals = ["ant", "bison", "camel", "duck", "elephant"];
+
+// console.log(animals.slice(2));
+// // Expected output: Array ["camel", "duck", "elephant"]
+
+// console.log(animals.slice(2, 4));
+// // Expected output: Array ["camel", "duck"]
+
+// const myStudent2 = ["Hoang", "Cuong", "Dung", "Duyen", "Cuong", "Hai"];
+// console.log(myStudent2);
+// myStudent2.splice(1, 2, "Donal Trump", "Edison", "Einstein");
+// console.log(myStudent2);
+
+// const yourStudents = ["Hung", "A Su"];
+
+// const newStudents = myStudent2.concat(yourStudents);
+// console.log(newStudents);
+
+/**
+ * Object
+ *
+ */
+
+// const myInfor = {
+//   myName: "HoangNM",
+//   "my phone": "073547271",
+//   address: "HN",
+//   server: "TraiDat",
+//   mail: "hoangm62@fpt.edu.vn",
+//   gender: null,
+// };
+
+// console.log(myInfor);
+// console.log(myInfor.myName);
+// console.log(myInfor["my phone"]);
+
+// console.log(Object.keys(myInfor).length);
+// console.log(Object.values(myInfor));
+// console.log(myInfor.length);
