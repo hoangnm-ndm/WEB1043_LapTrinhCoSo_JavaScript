c8d578c21708c874360ccf37da71187d0c0133a5
hoangnm <hoangnm.ndm@gmail.com>
2023-03-22 13:35:25 +0700

    update lession 8

---
+++ lession-8-Timer/index.js
@@ -0,0 +1,49 @@
+// console.log("Công việc 1");
+// const setInterval = setTimeout(function () {
+//   console.log("ddsd");
+// }, 2000);
+
+// console.log("Công việc 3");
+
+// clearInterval(myInterval);
+
+// BT:
+// const products = [];
+// const productA = {
+//   name: "product A",
+//   cost: 200,
+//   model: "model A",
+//   quanlity: 30,
+// };
+
+// const productB = {
+//   name: "product B",
+//   cost: 200,
+//   model: "model B",
+//   quanlity: 1000,
+// };
+
+// const productC = {
+//   name: "product C",
+//   cost: 200,
+//   model: "model C",
+//   quanlity: 300,
+// };
+
+// products.splice(0, 0, productA, productB, productC);
+// console.log(products);
+// console.log(products[2].quanlity);
+
+// BT4
+
+// let d = new Date();
+
+// var now = new Date();
+// var h = now.getHours();
+// var m = now.getMinutes();
+// var s = now.getSeconds();
+// document.write(h + `:` + m + `:` + s);
+
+// setTimeout(function () {
+//   location.reload();
+// }, 1000);
