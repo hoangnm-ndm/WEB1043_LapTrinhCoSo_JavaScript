beabf122944e612ad2927978d52edda13dcb1bde
hoangnm <hoangnm.ndm@gmail.com>
2023-03-22 07:47:14 +0700

    lession 7

---
+++ lession-7-event-OOP/index.js
@@ -0,0 +1,69 @@
+// console.log("Chao ca lop!");
+
+// const name = prompt(`Bạn tên là gì?`);
+
+function sayHi() {
+  alert(`Xin chào các bạn!!!`);
+}
+
+// console.log("viec 1");
+// setTimeout(() => {
+//   console.log("viec 2");
+// }, 3000);
+// console.log("viec 3");
+
+// const myClass = [
+//   { name: "Cuong", gender: "male" },
+//   {
+//     name: "Thoa",
+//     gender: "female",
+//   },
+// ];
+
+// for (student of myClass) {
+//   console.log(student.name);
+// }
+// for (let i = 0; i < myClass.length; i++) {
+//   console.log(myClass[i].name);
+// }
+
+// const myName = "Hoang";
+
+// for (character of myName) {
+//   console.log(character);
+// }
+
+// function Cat(name, weight, color, breed, type) {
+//   this.name = name;
+//   this.weight = weight;
+//   this.color = color;
+//   this.breed = breed;
+//   this.type = type;
+//   this.sua = function () {
+//     return "hihi";
+//   };
+// }
+// let dog1 = new Dog("Jacky", 0.8, "brown", "chihuahua teacup", "short hair");
+// let dog2 = new Dog("John", 1, "brown", "chihuahua mini", "long hair");
+
+// console.log(dog1);
+// console.log(dog2);
+
+const myInfor = {
+  name: "Hoang",
+  age: 18,
+  address: "HN",
+  hobbies: ["code", "coffee", "read", "cooking", "films"],
+  skills: [
+    "javascript",
+    "php",
+    "ruby",
+    "golang",
+    "react",
+    "angular",
+    "mongodb",
+    "nodejs",
+    "rust",
+    "MySQL",
+  ],
+};
