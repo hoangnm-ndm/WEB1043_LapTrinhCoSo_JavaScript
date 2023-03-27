a8d274e6edbac1ef2349da83db6245dbd0820c52
hoangnm <hoangnm.ndm@gmail.com>
2023-03-27 16:42:38 +0700

    Huong dan config format code = prettier

--- myCV/index.js
+++ myCV/index.js
@@ -3,3 +3,8 @@ function darkMode() {
   bodyElement.style.color = "#ccc";
   bodyElement.style.backgroundColor = "black";
 }
+
+// B1: Setting -> auto save -> onWindowChange
+// B2: Setting -> format on save
+// B3: Extension -> Prettier - Code Formatter
+// B4: Setting -> Default Formatter -> Prettier
