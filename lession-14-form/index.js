ae802d8d795b532ea5e16877f961bb2ae707aaea
hoangnm <hoangnm.ndm@gmail.com>
2023-04-03 09:17:47 +0700

    add BTVN

---
+++ lession-14-form/index.js
@@ -0,0 +1,11 @@
+/**
+ * BTVN:
+ * Từ form đã cho, khi ấn nút "Send" thì in ra màn hình console.log({username: "", email: "", password: ""})
+ * VD:
+ */
+
+const user = {
+  username: "hoangnnm",
+  email: "hoang123@gmail.com",
+  password: "123456",
+};
