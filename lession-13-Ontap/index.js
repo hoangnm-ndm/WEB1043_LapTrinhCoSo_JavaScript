63b585f5d39fcd60957d0fbf260be43db2b57d44
hoangnm <hoangnm.ndm@gmail.com>
2023-04-04 13:48:23 +0700

    add btvn

---
+++ lession-13-Ontap/index.js
@@ -0,0 +1,11 @@
+/**
+ * 1. Tạo một form đăng ký tài khoản mới bao gồm username, email, password.
+ * 2. Khi blur khỏi ô password, kiểm tra xem password cần phải có ít nhất 8 ký tự, nếu không có: in ra màn hình cảnh báo.
+ * 3. Khi ấn nút Send -> console.log() ra một object như bên dưới
+ */
+
+const user = {
+  username: "username do nguoi dung nhap vao",
+  email: "email do nguoi dung nhap vao",
+  password: "password do nguoi dung nhap vao",
+};
