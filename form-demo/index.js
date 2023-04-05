f2d1cf665439563900e2338f633caf1b2210e11f
hoangnm <hoangnm.ndm@gmail.com>
2023-04-05 12:26:15 +0700

    them de bai cho form-demo

---
+++ form-demo/index.js
@@ -0,0 +1,13 @@
+/**
+ * 1. Kiem tra xem username >= 8 ky tu
+ * 2. Kiem tra xem password >= 6 ky tu
+ * 3. Sau khi kiem tra xong thi lay thong tin cua nguoi dung luu vao trong object user = {}
+ */
+
+const user = {
+  username: "",
+  email: "",
+  password: "",
+};
+
+console.log(user);
